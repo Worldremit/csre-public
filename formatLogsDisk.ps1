e1291215c92dc72c9be3047113f6664b829031ca
Chris Hudson <chudson@worldremit.com>
2018-03-12 11:17:56 +0000

    generic disk format script

---
+++ formatLogsDisk.ps1
@@ -0,0 +1,12 @@
+﻿# Generic script to format the first available disk
+param (
+    [string]$driveLetter = 'L',
+    [string]$driveLabel = 'logsDisk'
+)
+
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+
+$disks[0] |
+Initialize-Disk -PartitionStyle MBR -PassThru |
+New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "$driveLabel" -Confirm:$false -Force
