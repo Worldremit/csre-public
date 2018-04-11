6aed8dd3d8551017877f8b33a7471ed584bbac3c
Chris Hudson <chudson@worldremit.com>
2018-04-11 10:40:01 +0100

    added main script for setting up sql vms

---
+++ scripts/setup_sql_vm.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [string]$dynaTraceId = "#{dynaTraceId}",
+    [string]$dynaTraceToken = "#{dynaTraceToken}",
+    [string]$msi = "#{msiUrl}"
+)
+
+Invoke-Expression $PSScriptRoot\install-dynatrace.ps1 $dynaTraceId $dynaTraceToken $msi
+
+Invoke-Expression $PSScriptRoot\install-failoverclustering.ps1
