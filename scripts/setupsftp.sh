3b6622161d32a274fadf4cb53f7eb8013a399b4e
MichaelEmbarck <membarck@worldremit.com>
2018-08-14 16:59:29 +0100

    Update setupsftp.sh

--- scripts/setupsftp.sh
+++ scripts/setupsftp.sh
@@ -10,7 +10,7 @@ echo "" >> /etc/ssh/sshd_config
 echo "#Subsystem sftp internal-sftp" >> /etc/ssh/sshd_config
 echo "X11Forwarding no" >> /etc/ssh/sshd_config
 echo "Match group ftpaccess" >> /etc/ssh/sshd_config
-echo "ChrootDirectory %h" >> /etc/ssh/sshd_config
+echo "ChrootDirectory /home/%u/upload" >> /etc/ssh/sshd_config
 echo "AllowTcpForwarding no" >> /etc/ssh/sshd_config
 echo "ForceCommand internal-sftp" >> /etc/ssh/sshd_config
 
@@ -19,95 +19,95 @@ systemctl restart ssh #-- restart to pick up config changes from sshd_config upd
 adduser dev-testuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo dev-testuser:$1) | chpasswd
 usermod -a -G ftpaccess dev-testuser
-chmod u=rwx,g=rx,o=rx /home/dev-testuser
+chown root:root /home/dev-testuser
+chmod go-w /home/dev-testuser
 mkdir /home/dev-testuser/upload
-chown root:root /home/dev-testuser/upload
-chown -R dev-testuser:ftpaccess /home/dev-testuser/upload
+chown dev-testuser:ftpaccess /home/dev-testuser/upload
 
 adduser dev-bhd --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo dev-bhd:$1) | chpasswd
 usermod -a -G ftpaccess dev-bhd
-chmod u=rwx,g=rx,o=rx /home/dev-bhd
+chown root:root /home/dev-bhd
+chmod go-w /home/dev-bhd
 mkdir /home/dev-bhd/upload
-chown root:root /home/dev-bhd/upload
-chown -R dev-bhd:ftpaccess /home/dev-bhd/upload
+chown dev-bhd:ftpaccess /home/dev-bhd/upload
 
 adduser dev-bolivariano --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo dev-bolivariano:$1) | chpasswd
 usermod -a -G ftpaccess dev-bolivariano
-chmod u=rwx,g=rx,o=rx /home/dev-bolivariano
-mkdir /home/dev-bolivariano/upload 
-chown root:root /home/dev-bolivariano/upload
-chown -R dev-bolivariano:ftpaccess /home/dev-bolivariano/upload
+chown root:root /home/dev-bolivariano
+chmod go-w /home/dev-bolivariano
+mkdir /home/dev-bolivariano/upload
+chown dev-bolivariano:ftpaccess /home/dev-bolivariano/upload
 
 adduser dev-davivienda --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo dev-davivienda:$1) | chpasswd
 usermod -a -G ftpaccess dev-davivienda
-chmod u=rwx,g=rx,o=rx /home/dev-davivienda
+chown root:root /home/dev-davivienda
+chmod go-w /home/dev-davivienda
 mkdir /home/dev-davivienda/upload
-chown root:root /home/dev-davivienda/upload
-chown -R dev-davivienda:ftpaccess /home/dev-davivienda/upload
+chown dev-davivienda:ftpaccess /home/dev-davivienda/upload
 
 adduser int-testuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo int-testuser:$1) | chpasswd
 usermod -a -G ftpaccess int-testuser
-chmod u=rwx,g=rx,o=rx /home/int-testuser
+chown root:root /home/int-testuser
+chmod go-w /home/int-testuser
 mkdir /home/int-testuser/upload
-chown root:root /home/int-testuser/upload
-chown -R int-testuser:ftpaccess /home/int-testuser/upload
+chown int-testuser:ftpaccess /home/int-testuser/upload
 
 adduser int-bhd --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo int-bhd:$1) | chpasswd
 usermod -a -G ftpaccess int-bhd
-chmod u=rwx,g=rx,o=rx /home/int-bhd
+chown root:root /home/int-bhd
+chmod go-w /home/int-bhd
 mkdir /home/int-bhd/upload
-chown root:root /home/int-bhd/upload
-chown -R int-bhd:ftpaccess /home/int-bhd/upload
+chown int-bhd:ftpaccess /home/int-bhd/upload
 
 adduser int-bolivariano --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo int-bolivariano:$1) | chpasswd
 usermod -a -G ftpaccess int-bolivariano
-chmod u=rwx,g=rx,o=rx /home/int-bolivariano
+chown root:root /home/int-bolivariano
+chmod go-w /home/int-bolivariano
 mkdir /home/int-bolivariano/upload
-chown root:root /home/int-bolivariano/upload
-chown -R int-bolivariano:ftpaccess /home/int-bolivariano/upload
+chown int-bolivariano:ftpaccess /home/int-bolivariano/upload
 
 adduser int-davivienda --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo int-davivienda:$1) | chpasswd
 usermod -a -G ftpaccess int-davivienda
 chown root:root /home/int-davivienda
-chmod u=rwx,g=rx,o=rx /home/int-davivienda
+chmod go-w /home/int-davivienda
 mkdir /home/int-davivienda/upload
-chown -R int-davivienda:ftpaccess /home/int-davivienda/upload
+chown int-davivienda:ftpaccess /home/int-davivienda/upload
 
 adduser uat-testuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo uat-testuser:$1) | chpasswd
 usermod -a -G ftpaccess uat-testuser
-chmod u=rwx,g=rx,o=rx /home/uat-testuser
+chown root:root /home/uat-testuser
+chmod go-w /home/uat-testuser
 mkdir /home/uat-testuser/upload
-chown root:root /home/uat-testuser/upload
-chown -R uat-testuser:ftpaccess /home/uat-testuser/upload
+chown uat-testuser:ftpaccess /home/uat-testuser/upload
 
 adduser uat-bhd --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo uat-bhd:$1) | chpasswd
 usermod -a -G ftpaccess uat-bhd
-chmod u=rwx,g=rx,o=rx /home/uat-bhd
+chown root:root /home/uat-bhd
+chmod go-w /home/uat-bhd
 mkdir /home/uat-bhd/upload
-chown root:root /home/uat-bhd/upload
-chown -R uat-bhd:ftpaccess /home/uat-bhd/upload
+chown uat-bhd:ftpaccess /home/uat-bhd/upload
 
 adduser uat-bolivariano --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo uat-bolivariano:$1) | chpasswd
 usermod -a -G ftpaccess uat-bolivariano
-chmod u=rwx,g=rx,o=rx /home/uat-bolivariano
+chown root:root /home/uat-bolivariano
+chmod go-w /home/uat-bolivariano
 mkdir /home/uat-bolivariano/upload
-chown root:root /home/uat-bolivariano/upload
-chown -R uat-bolivariano:ftpaccess /home/uat-bolivariano/upload
+chown uat-bolivariano:ftpaccess /home/uat-bolivariano/upload
 
 adduser uat-davivienda --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 (echo uat-davivienda:$1) | chpasswd
 usermod -a -G ftpaccess uat-davivienda
-chmod u=rwx,g=rx,o=rx /home/uat-davivienda
+chown root:root /home/uat-davivienda
+chmod go-w /home/uat-davivienda
 mkdir /home/uat-davivienda/upload
-chown root:root /home/uat-davivienda/upload
-chown -R uat-davivienda:ftpaccess /home/uat-davivienda/upload
+chown uat-davivienda:ftpaccess /home/uat-davivienda/upload
