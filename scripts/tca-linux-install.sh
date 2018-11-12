16bcaa9864bc0ffa7dcba15f6f4f342c505f4b75
Tzanko Tzankov <ttzankov@wrlonsp60.wrhammersmith.worldremit.com>
2018-11-12 17:29:49 +0000

    updates to TC install

--- scripts/tca-linux-install.sh
+++ scripts/tca-linux-install.sh
@@ -3,3 +3,15 @@
 touch /opt/test
 
 echo "it works" > /opt/test
+
+cd /opt/
+
+wget https://teamcity.csre.worldremit.com/update/buildAgent.zip
+
+unzip /opt/buildAgent.zip
+
+ls -ltrh
+
+cd /opt/buildAgent/
+
+bash install.sh
